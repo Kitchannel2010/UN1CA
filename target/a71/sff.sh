46099560ff7256e61cb4fa6a8326795dccbfa1e4
Samuel Pascua <pascua.samuel.14@gmail.com>
2026-03-10 22:56:01 +0800

    a71: Enable extra brightness feature
    Signed-off-by: Samuel Pascua <pascua.samuel.14@gmail.com>

--- target/a71/sff.sh
+++ target/a71/sff.sh
@@ -23,3 +23,6 @@ SEC_FLOATING_FEATURE_LCD_CONFIG_HFR_SUPPORTED_REFRESH_RATE_NS=
 
 # Camera
 SEC_FLOATING_FEATURE_CAMERA_CONFIG_VENDOR_LIB_INFO=image_codec.samsung.v1,beauty.samsung.v4,face_landmark.arcsoft.v2_1,facial_attribute.samsung.v1,swuwdc.arcsoft.v1,event_detection.samsung.v2,food.samsung.v1,selfie_correction.samsung.v1,dual_bokeh.samsung.v1,single_bokeh.samsung.v2,image_enhance.arcsoft.v1,mfhdr.arcsoft.v1,llhdr.arcsoft.v1,scene_detection.samsung.v1,smart_scan.samsung.v2,human_tracking.arcsoft.v2_1,localtm.samsung.v1_1
+
+# Enable extra brightness feature
+SEC_FLOATING_FEATURE_LCD_SUPPORT_EXTRA_BRIGHTNESS=TRUE
