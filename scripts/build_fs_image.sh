19ee456533465033ba1948a4a554db05657aaa99
Kitchannel2010 <84881541+Kitchannel2010@users.noreply.github.com>
2026-03-22 23:02:19 +0000

    Update build_fs_image.sh

--- scripts/build_fs_image.sh
+++ scripts/build_fs_image.sh
@@ -26,7 +26,7 @@ BUILD_IMAGE_MKFS()
     local MANUAL_SPARSE=false
 
     # Avoid OOM errors when building as sparse image
-    if $SPARSE && [[ "$(awk '/MemTotal/ { print int ($2 / 1024) }' "/proc/meminfo")" -lt "10240" ]]; then
+    if $SPARSE; then
         SPARSE=false
         MANUAL_SPARSE=true
     fi
